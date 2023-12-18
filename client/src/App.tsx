7b587d74bb20f76c91e779283e7bd7bf29438736
ihorsharov <cookie50ds@gmail.com>
2023-12-18 16:16:52 +0200

    feature: add browser router with Home and Auth pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,15 @@
-function App() {
+import { Navigate, Route, Routes } from "react-router-dom";
+import Authentication from "./pages/authentication/Authentication";
+import Home from "./pages/home/Home";
 
+function App() {
   return (
-    <>
-      hw 
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/authentication" element={<Authentication />} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
