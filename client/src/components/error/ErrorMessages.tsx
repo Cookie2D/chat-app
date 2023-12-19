f8229017b4ddf935fc9053c29563c516f966ca24
ihorsharov <cookie50ds@gmail.com>
2023-12-19 15:39:06 +0200

    fix: error messaging component displaying

--- client/src/components/error/ErrorMessages.tsx
+++ client/src/components/error/ErrorMessages.tsx
@@ -1,4 +1,14 @@
 import { Typography } from "@mui/material";export default function ErrorMessages({ errors }: { errors: string[] }) {
+  if (!Array.isArray(errors)) {
+    return (
+      <>
+        <Typography variant="body2" color="error">
+          {errors}
+        </Typography>
+      </>
+    );
+  }
+
   return (
     <>
       {errors.map((message, index) => (
