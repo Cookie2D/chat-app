1ba09450984a18417a239cece99d6f7ee2dba044
ihorsharov <cookie50ds@gmail.com>
2023-12-21 11:04:30 +0200

    refactor: remove unused component from authentication

--- client/src/pages/authentication/Authentication.tsx
+++ client/src/pages/authentication/Authentication.tsx
@@ -5,7 +5,6 @@ import Box from "@mui/material/Box";
 import HttpsIcon from "@mui/icons-material/Https";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { Controller, useForm } from "react-hook-form";
@@ -15,8 +14,6 @@ import { useNavigate } from "react-router-dom";
 import { useAppDispatch } from "../../store/hooks";
 import { setUser } from "../../store/slices/authSlice";
 import ErrorMessages from "../../components/error/ErrorMessages";
-
-const defaultTheme = createTheme();
 interface FormData {
   name: string;
   password: string;
@@ -52,83 +49,83 @@ export default function SignIn() {
   };
 
   useEffect(() => {
-    if (isSuccess) {
-      dispatch(
-        setUser({
-          id: data.user.id,
-          name: data.user.name,
-          role: data.user.roleId,
-          token: data.access_token,
-          color: data.user.color,
-        })
-      );
-      navigate("/chat");
+    if (!isSuccess) {
+      return;
     }
+
+    dispatch(
+      setUser({
+        id: data.user.id,
+        name: data.user.name,
+        role: data.user.roleId,
+        token: data.access_token,
+        color: data.user.color,
+      })
+    );
+    navigate("/chat");
   }, [isSuccess, navigate, dispatch, data]);
 
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
-            <HttpsIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit(onSubmit)} noValidate sx={{ mt: 1 }}>
-            <Controller
-              name="name"
-              control={control}
-              defaultValue=""
-              render={({ field }) => (
-                <TextField
-                  id="name"
-                  error={!!errors.name}
-                  helperText={errors.name?.message}
-                  {...field}
-                  margin="normal"
-                  required
-                  fullWidth
-                  label="User Name"
-                  autoComplete="email"
-                  autoFocus
-                />
-              )}
-            />
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
+          <HttpsIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit(onSubmit)} noValidate sx={{ mt: 1 }}>
+          <Controller
+            name="name"
+            control={control}
+            defaultValue=""
+            render={({ field }) => (
+              <TextField
+                id="name"
+                error={!!errors.name}
+                helperText={errors.name?.message}
+                {...field}
+                margin="normal"
+                required
+                fullWidth
+                label="User Name"
+                autoComplete="email"
+                autoFocus
+              />
+            )}
+          />
 
-            <Controller
-              name="password"
-              control={control}
-              defaultValue=""
-              render={({ field }) => (
-                <TextField
-                  margin="normal"
-                  required
-                  fullWidth
-                  type="password"
-                  label="Password"
-                  error={!!errors.password}
-                  helperText={errors.password?.message}
-                  autoComplete="current-password"
-                  {...field}
-                />
-              )}
-            />
-            {isError && <ErrorMessages errors={error?.data?.message} />}
-            <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
-              Sign In
-            </Button>
-          </Box>
+          <Controller
+            name="password"
+            control={control}
+            defaultValue=""
+            render={({ field }) => (
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                type="password"
+                label="Password"
+                error={!!errors.password}
+                helperText={errors.password?.message}
+                autoComplete="current-password"
+                {...field}
+              />
+            )}
+          />
+          {isError && <ErrorMessages errors={error?.data?.message} />}
+          <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+            Sign In
+          </Button>
         </Box>
-      </Container>
-    </ThemeProvider>
+      </Box>
+    </Container>
   );
 }
