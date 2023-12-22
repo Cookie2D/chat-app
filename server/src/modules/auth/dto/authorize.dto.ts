103ce143b6b6c8301e4615999c43f97bc8d98261
ihorsharov <cookie50ds@gmail.com>
2023-12-22 13:26:50 +0200

    feature: add more validations to user password

--- server/src/modules/auth/dto/authorize.dto.ts
+++ server/src/modules/auth/dto/authorize.dto.ts
@@ -1,3 +1,19 @@
-import { CreateUserDto } from 'src/modules/user/dto/create-user.dto';
+import { IsString, MinLength, Matches, IsNotEmpty } from 'class-validator';
 
-export class AuthorizeDto extends CreateUserDto {}
+export class AuthorizeDto {
+  @IsString()
+  @MinLength(3)
+  @Matches(/^[a-zA-Z0-9]+$/, {
+    message:
+      'Name must be at least 3 characters and contain only alphanumeric characters.',
+  })
+  name: string;
+
+  @IsNotEmpty()
+  @MinLength(8)
+  @Matches(/[A-Z]/, {
+    message: 'Password must contain at least one uppercase letter',
+  })
+  @IsString()
+  password: string;
+}
